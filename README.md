ec89c5387de9365e51446f2dc8f539a47ec139b4
zummon <zummon.space@gmail.com>
2023-04-28 20:27:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# foody-sweety-sveltekit
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-qvmrmz)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+
